e876da6cb4bfe9ea7382476b10d6fd9a7ebbe2c0
Hesham <heshammathana0@gmail.com>
2025-05-23 00:09:47 +0300

    Apply For upload Images

--- Taskk Management Application.PL/Controllers/EmployeeController.cs
+++ Taskk Management Application.PL/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Taskk_Management_Application.PL.Models;
 using TaskManagementApplication.BLL.Intefaces;
 using TaskManagementApplication.DAL.Models;
@@ -49,7 +50,45 @@ namespace Taskk_Management_Application.PL.Controllers
             }
             return View();
         }
-
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            ViewData["departments"] = await unionInterface.IdepartmentInterface.GetAllAsync();
+            ViewData["Tasks"] = await unionInterface.ITaskInterface.GetAllAsync();
+            var model = await unionInterface.IEmployeeInterface.GetByIdAsync(id);
+              var  MapingResult= mapper.Map<CreateEmployeeDto>(model);
+            if (model == null) return NotFound();
+            return View(MapingResult);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update([FromRoute] int id,CreateEmployeeDto createEmployeeDto)
+        {
+            //if(ModelState.IsValid)
+            //{
+           
+                if(createEmployeeDto.Image is not null &&createEmployeeDto.ImageName is not null)
+                {
+                    Helping.DocumentSetting.Delete(createEmployeeDto.ImageName, "Images");
+                   createEmployeeDto.ImageName= Helping.DocumentSetting.Upload(createEmployeeDto.Image, "Images");
+                }
+                if(createEmployeeDto.Image is not null)
+                {
+                    createEmployeeDto.ImageName =  Helping.DocumentSetting.Upload(createEmployeeDto.Image, "Images");
+                }
+                  var Employee = mapper.Map<Employee>(createEmployeeDto);
+                  Employee.Id = id;
+            unionInterface.IEmployeeInterface.Update(Employee);
+                var Result= await unionInterface.Complete();
+                if (Result > 0)
+                {
+                    return RedirectToAction("GetAll");
+                }
+                
+            //}
+            ViewData["departments"] = await unionInterface.IdepartmentInterface.GetAllAsync();
+            ViewData["Tasks"] = await unionInterface.ITaskInterface.GetAllAsync();
+            return View(createEmployeeDto);
+        }
 
     }
 }
--- Taskk Management Application.PL/Models/CreateEmployeeDto.cs
+++ Taskk Management Application.PL/Models/CreateEmployeeDto.cs
@@ -1,29 +1,40 @@
-﻿using TaskManagementApplication.DAL.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagementApplication.DAL.Models;
 
 namespace Taskk_Management_Application.PL.Models
 {
     public class CreateEmployeeDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Range(18, 65, ErrorMessage = "Age must be between 18 and 65")]
         public int? Age { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
         public string ImageName { get; set; }
         public bool IsDeleted { get; set; }
         public bool IsActive { get; set; }
+        [Required(ErrorMessage = "Address is required")]
         public string Addres { get; set; }
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Hiring date is required")]
         public DateTime HiringDate { get; set; }
 
 
 
 
-
+        [Required(ErrorMessage = "Department is required")]
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
 
 
 
         public IFormFile Image {  get; set; }
+        [Required(ErrorMessage = "Task is required")]
         public int TaskId { get; set; }
         public Tasks Tasks { get; set; }
     }
